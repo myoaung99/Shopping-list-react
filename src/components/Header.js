d48f63fb82b54836f4867721b4350998ff8c0410
MyoMyintAung <myomyintaung064@gmail.com>
2022-07-04 15:07:13 +0630

    display total items from shop-list context in header badge

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { ShoppingListContext } from "./../store/shop-list-context";
 
 const Header = () => {
+  const shopCtx = useContext(ShoppingListContext);
+  const items = shopCtx.listItems;
+  let itemAmount;
+
+  if (items && items.length > 0) {
+    itemAmount = items.reduce((total, item) => total + item.quantity, 0);
+  }
+
   return (
     <header className="fixed top-0 w-full bg-teal-900 z-10 border-y md:border shadow-md ">
       <div className="h-20 container mx-auto flex items-center justify-between text-white px-5">
         <h1 className="text-white text-5xl">Make a List</h1>
         <h3>
-          <button className="border py-1 px-2 rounded-full inline">3</button>{" "}
+          <button className="border py-1 px-2 rounded-full inline">
+            {itemAmount}
+          </button>{" "}
           <span className="hidden md:inline">items in list</span>
         </h3>
       </div>
