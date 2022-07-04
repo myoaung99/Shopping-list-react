e518bbb933d92d5924284aec3ef723a220553923
MyoMyintAung <myomyintaung064@gmail.com>
2022-07-04 15:09:46 +0630

    display correct total amount

--- src/components/ShopList.js
+++ src/components/ShopList.js
@@ -6,6 +6,7 @@ const ShopList = () => {
   const shopCtx = useContext(ShoppingListContext);
 
   const itemsList = shopCtx.listItems;
+  const totalAmount = shopCtx.totalAmount.toFixed(2);
   return (
     <div className="w-full md:w-1/2 md:border relative h-screen py-28 px-10 text-white overflow-y-scroll mt-32 md:mt-0">
       <ul className="p-0">
@@ -21,8 +22,10 @@ const ShopList = () => {
       </ul>
 
       <div className="mt-3 flex justify-between items-center">
-        <h2>Total</h2>
-        <p className="text-2xl underline underline-offset-8 ">$ 434.23</p>
+        <h2 className="text-3xl">Total</h2>
+        <p className="text-2xl underline underline-offset-8 ">
+          $ {totalAmount}
+        </p>
       </div>
     </div>
   );
